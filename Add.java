8face886a3d6509af7159947295b5b55628f00d5
sowmya <gonurusowmya@gmail.com>
2025-02-01 15:18:04 +0530

    Added add.java

---
+++ Add.java
@@ -0,0 +1,15 @@
+//Addition
+import java.util.*;
+class Add
+{
+public static void main(String[] abc)
+{
+int a,b,c;
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter a,b values");
+a=obj.nextInt();
+b=obj.nextInt();
+c=a+b;
+System.out.print("result="+c);
+}
+}
